c98aa0587dbaa3b3d7343c03fc8e25cad8a2d6a2
Marko Tomljanovic <markot@windowslive.com>
2020-11-13 00:30:10 +0100

    test

---
+++ src/components/JobCard.vue
@@ -0,0 +1,11 @@
+<template>
+  
+ <b-col>
+      <b-card title="This is a card" img-src="http://picsum.photos/400/300/?image=25" img-top style="max-width:20rem" class="mb-2">
+        <b-card-text>
+        Nesto tekt kao samo da proba ovo ono</b-card-text>
+        <b-button variant="primary">Apply</b-button>
+      </b-card>
+    </b-col>
+
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <b-container>
+  <b-row align-center="center">
+    <job-card></job-card>
+    <job-card></job-card>
+    <job-card></job-card>
+    <job-card></job-card>
+
+    <!-- <b-col md="3">
+      <b-card title="This is a card" img-src="http://picsum.photos/400/300/?image=25" img-top style="max-width:20rem" class="mb-2">
+        <b-card-text>
+        Nesto tekt kao samo da proba ovo ono</b-card-text>
+        <b-button variant="primary">Apply</b-button>
+      </b-card>
+    </b-col>
+
+
+    <b-col md="3">
+      <b-card title="This is a card" img-src="http://picsum.photos/400/300/?image=25" img-top style="max-width:20rem" class="mb-2">
+        <b-card-text>
+        Nesto tekt kao samo da proba ovo ono</b-card-text>
+        <b-button variant="primary">Apply</b-button>
+      </b-card>
+    </b-col>
+
+
+    <b-col md="3">
+      <b-card title="This is a card" img-src="http://picsum.photos/400/300/?image=25" img-top style="max-width:20rem" class="mb-2">
+        <b-card-text>
+        Nesto tekt kao samo da proba ovo ono</b-card-text>
+        <b-button variant="primary">Apply</b-button>
+      </b-card>
+    </b-col>
+
+
+    <b-col md="3">
+      <b-card title="This is a card" img-src="http://picsum.photos/400/300/?image=25" img-top style="max-width:20rem" class="mb-2">
+        <b-card-text>
+        Nesto tekt kao samo da proba ovo ono</b-card-text>
+        <b-button variant="primary">Apply</b-button>
+      </b-card>
+    </b-col> -->
+    
+  </b-row>
+</b-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import JobCard from "@/components/JobCard.vue";
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {"job-card": JobCard }
 }
 </script>
