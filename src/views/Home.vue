c8133235d438ea089e07d01005056f0fcbbd3f48
Marko Tomljanovic <markot@windowslive.com>
2020-11-13 00:32:09 +0100

    delete

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,40 +6,7 @@
     <job-card></job-card>
     <job-card></job-card>
 
-    <!-- <b-col md="3">
-      <b-card title="This is a card" img-src="http://picsum.photos/400/300/?image=25" img-top style="max-width:20rem" class="mb-2">
-        <b-card-text>
-        Nesto tekt kao samo da proba ovo ono</b-card-text>
-        <b-button variant="primary">Apply</b-button>
-      </b-card>
-    </b-col>
-
-
-    <b-col md="3">
-      <b-card title="This is a card" img-src="http://picsum.photos/400/300/?image=25" img-top style="max-width:20rem" class="mb-2">
-        <b-card-text>
-        Nesto tekt kao samo da proba ovo ono</b-card-text>
-        <b-button variant="primary">Apply</b-button>
-      </b-card>
-    </b-col>
-
-
-    <b-col md="3">
-      <b-card title="This is a card" img-src="http://picsum.photos/400/300/?image=25" img-top style="max-width:20rem" class="mb-2">
-        <b-card-text>
-        Nesto tekt kao samo da proba ovo ono</b-card-text>
-        <b-button variant="primary">Apply</b-button>
-      </b-card>
-    </b-col>
-
-
-    <b-col md="3">
-      <b-card title="This is a card" img-src="http://picsum.photos/400/300/?image=25" img-top style="max-width:20rem" class="mb-2">
-        <b-card-text>
-        Nesto tekt kao samo da proba ovo ono</b-card-text>
-        <b-button variant="primary">Apply</b-button>
-      </b-card>
-    </b-col> -->
+ 
     
   </b-row>
 </b-container>
