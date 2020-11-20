2b617a0b0f1d4a539886a74d5033d29b2063c82c
Marko Tomljanovic <markot@windowslive.com>
2020-11-20 16:24:00 +0100

    view

--- src/components/TopHeader.vue
+++ src/components/TopHeader.vue
@@ -1,44 +1,16 @@
 <template>
 <div>
   <b-navbar toggleable="lg" type="dark" variant="#E6E6FA" >
-    <b-navbar-brand style="font-size: 26px; color: #f44556" href="#">NavBar</b-navbar-brand>
+    <b-navbar-brand style="font-size: 26px; color: #306b8e" to="/">Home</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav style="font-size: 20px">
-        <b-nav-item to="/"><a style="color: #f44556">Home</a> </b-nav-item>
-        <b-nav-item to="/"><a style="color: #f44556">Education</a> </b-nav-item>
-        <b-nav-item to="/"><a style="color: #f44556">Features</a> </b-nav-item>
-        <b-nav-item to="/"><a style="color: #f44556">json</a> </b-nav-item>
+         <b-nav-item to="/education"><a style="color: #306b8e">Education</a> </b-nav-item>
+        <b-nav-item to="/feauters"><a style="color: #306b8e">Features</a> </b-nav-item>
+        <b-nav-item to="/jonson"><a style="color: #306b8e">json</a> </b-nav-item>
         
-        
-      </b-navbar-nav>
-
-      
-
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-        <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-        </b-nav-form>
-
-        <b-nav-item-dropdown text="Lang" right>
-          <b-dropdown-item href="#">EN</b-dropdown-item>
-          <b-dropdown-item href="#">ES</b-dropdown-item>
-          <b-dropdown-item href="#">RU</b-dropdown-item>
-          <b-dropdown-item href="#">FA</b-dropdown-item>
-        </b-nav-item-dropdown>
-
-        <b-nav-item-dropdown right>
-          <!-- Using 'button-content' slot -->
-          <template #button-content>
-            <em>User</em>
-          </template>
-          <b-dropdown-item href="#">Profile</b-dropdown-item>
-          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-        </b-nav-item-dropdown>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
