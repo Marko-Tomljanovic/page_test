1f24255509404ab686ebc34b9fac8a299e261927
Marko Tomljanovic <markot@windowslive.com>
2020-12-06 23:24:42 +0100

    <br>

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
       <router-link to="/about">About</router-link> -->
       <transition name="fade">
         <router-view/><br>
-      </transition>
+      </transition><br>
         <bootom></bootom>
   </div>
 </template>
