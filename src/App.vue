6c3008cf12c48aedb11e50b885b153e05b7da781
Marko Tomljanovic <markot@windowslive.com>
2021-06-01 23:02:14 +0200

    aa

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,39 @@
 <template>
-  <div> 
-      <top-header></top-header>
-      <!-- <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> -->
-      <transition name="fade">
-        <router-view/><br>
-      </transition><br>
-        <bootom></bootom>
+  <div>
+    <top-header></top-header>
+    <transition name="fade"> <router-view /><br /> </transition><br />
+    <bootom></bootom>
   </div>
 </template>
 
-
-
 <script>
-import TopHeader from '@/components/TopHeader.vue';
-import Bootom from '@/components/Bootom.vue';
+import TopHeader from "@/components/TopHeader.vue";
+import Bootom from "@/components/Bootom.vue";
 export default {
-  name: 'app',
-  components:{
-    'top-header': TopHeader,
-    'bootom': Bootom
-  }
-}
+  name: "app",
+  components: {
+    "top-header": TopHeader,
+    bootom: Bootom,
+  },
+};
 </script>
 
-
-
 <style>
-body{
+body {
   background-color: #2677a7;
 }
-.fade-enter-active, .fade-leave-active {
+.fade-enter-active,
+.fade-leave-active {
   transition: opacity 0.5s ease;
 }
 
-.fade-enter-from, .fade-leave-to {
+.fade-enter-from,
+.fade-leave-to {
   opacity: 0;
 }
-.mojstil{
+.mojstil {
   color: #ffffff;
-  font-family: "Trebuchet MS", "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", Tahoma, sans-serif;
+  font-family: "Trebuchet MS", "Lucida Grande", "Lucida Sans Unicode",
+    "Lucida Sans", Tahoma, sans-serif;
 }
-
 </style>
-
-
-
